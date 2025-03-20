064854a7da81550043dcef51da019d87b7e237cd
Conscientious Technologies <167194750+CT-Dev-User@users.noreply.github.com>
2025-03-20 13:16:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import { CourseContextProvider } from './context/CourseContext.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 
 // export const server = 'http://localhost:3000'
-export const server = 'https://techmomentum-lms1.vercel.app'
+// export const server = 'https://techmomentum-lms1.vercel.app'
+export const server = 'https://tm-lms-backend.vercel.app'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
